c2dbace1cfaceb8583a39312af5387f17d9aa4f4
Birgül Özhan <birgulozhan9@gmail.com>
2026-04-26 13:54:57 +0300

    sayfalar oluşturuldu

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
+import { Routes, Route } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { Store } from "./pages/Store";
+import { About } from "./pages/About";
+
+
 function App() {
-  return (
-    <div>
-      Hi!
+  return(
+    <div className="max-w-7xl mb-3 mx-auto p-4 sm:px-6 lg:px-8">
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/store" element={<Store />}></Route>
+        <Route path="/about" element={<About />}></Route>
+      </Routes>
     </div>
   )
 }
 
-export default App
+export default App;
---
+++ src/pages/Store.tsx
@@ -0,0 +1,8 @@
+export function Store() {
+  return (
+    <>
+      <h1>Store Page</h1>
+      <div>Store Content</div>
+    </>
+  );
+}
